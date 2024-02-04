55544502018e1086a37526fa361d879b10028c93
msntts <terzonome-github@yahoo.co.jp>
2024-02-04 22:55:44 +0900

    fix swagger-initializer.js

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -15,18 +15,5 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout"
   });
-   <script>
-     window.onload = () => {
-       window.ui = SwaggerUIBundle({
-         url: 'openapi.json',
-         dom_id: '#swagger-ui',
-         presets: [
-           SwaggerUIBundle.presets.apis,
-           SwaggerUIStandalonePreset
-         ],
-         layout: "StandaloneLayout",
-       });
-     };
-   </script>
   //</editor-fold>
 };
